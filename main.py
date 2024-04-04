e9e89fab48346e71a9af565c2fa464e4af71b67d
bleidd <snieg7@gmail.com>
2024-04-04 23:51:22 +0200

    main()

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from listener import Listener
 from gen.asdListener import asdListener
 from antlr4 import *
 
-code = InputStream("x = 10 print(x)")
-lexer = asdLexer(input=code)
-tokens = CommonTokenStream(lexer)
-parser = asdParser(tokens)
-tree = parser.prog()
-listener = Listener()
-walker = ParseTreeWalker()
-walker.walk(listener, tree)
 
+def main():
+    code = InputStream("x = 10 print(x)")
+    lexer = asdLexer(input=code)
+    tokens = CommonTokenStream(lexer)
+    parser = asdParser(tokens)
+    tree = parser.prog()
+    listener = Listener()
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+
+
+if __name__ == "__main__":
+    main()
